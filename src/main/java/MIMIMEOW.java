75cde0b348563d6ddf3a2944e5fbb4edf00fe98d
XuanYou <tanxuanyou2588@gmail.com>
2026-08-22 23:34:32 +0800

    Added simple user greeting and exit code

--- src/main/java/MIMIMEOW.java
+++ src/main/java/MIMIMEOW.java
@@ -10,5 +10,16 @@ public class MIMIMEOW {
                 + " ╚═╝     ╚═╝╚═╝╚═╝     ╚═╝╚═╝╚═╝     ╚═╝╚══════╝ ╚═════╝  ╚══╝╚══╝ \n"
                 + "      ( o.o ) | | (^._.^) | | (｡♥‿♥｡) | | (^._.^) | | ( o.o )      \n";
         System.out.println(banner);
+        System.out.println("Hello! I'm MIMIMEOW.");
+        System.out.println("What can I do for you?");
+        for (int i = 0; i < 16; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+        for (int i = 0; i < 16; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }

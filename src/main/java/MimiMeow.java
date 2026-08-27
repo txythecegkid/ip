209fd52ff01f3ac3d0aa426fba0121243e775b2a
XuanYou <tanxuanyou2588@gmail.com>
2026-08-27 17:05:53 +0800

    Add task storage and list command

--- src/main/java/MimiMeow.java
+++ src/main/java/MimiMeow.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class MimiMeow {
-    public static void main(String[] args) {
+    private static final String[] storedUI = new String[100];
+    private static int storedUICount = 0;
 
+    public static void main(String[] args) {
         String userInput;
         String banner =
                   "      ( o.o ) | | (^._.^) | | (｡♥‿♥｡) | | (^._.^) | | ( o.o )      \n"
@@ -14,30 +16,59 @@ public class MimiMeow {
                 + " ╚═╝     ╚═╝╚═╝╚═╝     ╚═╝╚═╝╚═╝     ╚═╝╚══════╝ ╚═════╝  ╚══╝╚══╝ \n"
                 + "      ( o.o ) | | (^._.^) | | (｡♥‿♥｡) | | (^._.^) | | ( o.o )      \n";
         System.out.println(banner);
-        printMimi("Hello! I'm MimiMeow.");
-        printMimi("What can I do for you?");
-        printMimi("─".repeat(60));
-        printMimi("");
+        printwithIndent("Hello! I'm MimiMeow.");
+        printwithIndent("What can I do for you?");
+        printwithIndent("─".repeat(60));
+        printwithIndent("");
         Scanner in = new Scanner(System.in);
 
         while (true) {
             userInput = in.nextLine();
-            printMimi("─".repeat(60));
+            printwithIndent("─".repeat(60));
             if (userInput.equals("bye")) {
                 break;
             }
-            printMimi("(^._.^) says:");
-            printMimi(userInput);
-            printMimi("─".repeat(60));
+            if (userInput.equals("list")) {
+                printSavedUI();
+            }else{
+                appendStoredUI(userInput);
+                printwithIndent("(^._.^) says:");
+                printMimiReply(userInput);
+            }
+
+            printwithIndent("─".repeat(60));
         }
 
-        printMimi("Bye. Hope to see you again soon!");
-        printMimi("─".repeat(60));
-        printMimi("");
+        printwithIndent("Bye. Hope to see you again soon!");
+        printwithIndent("─".repeat(60));
+        printwithIndent("");
     }
 
 
-    private static void printMimi(String message) {
+    private static void printwithIndent(String message) {
         System.out.println("    " + message);
+    }
+
+    private static void printMimiReply(String message) {
+        System.out.println("    added: " + message);
+    }
+
+    private static void printSavedUI() {
+        for (int i = 0; i < storedUICount; i++) {
+            printwithIndent((i+1) + "." + storedUI[i]);
+        }
+    }
+
+    public static String [] getStoredUI() {
+        return storedUI;
+    }
+
+    public static void appendStoredUI(String message) {
+        if (storedUICount < storedUI.length) {
+            storedUI[storedUICount] = message;
+            storedUICount++;
+        } else {
+            System.out.println("Storage is full.");
+        }
     }
 }

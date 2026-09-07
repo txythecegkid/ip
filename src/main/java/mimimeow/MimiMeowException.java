71965136bfe5041776f46615a8f9148d37694e1d
XuanYou <tanxuanyou2588@gmail.com>
2026-09-07 18:40:35 +0800

    Revert "Handle invalid user input with exceptions"
    This reverts commit e8464ef7276dcfe285881768fc01a4b4c4e03fa8.

--- src/main/java/mimimeow/MimiMeowException.java
+++
@@ -1,9 +0,0 @@
-package mimimeow;
-
-/** Represents an error caused by invalid input given to MimiMeow. */
-public class MimiMeowException extends RuntimeException {
-    /** Creates an exception with the specified user-facing message. */
-    public MimiMeowException(String message) {
-        super(message);
-    }
-}
